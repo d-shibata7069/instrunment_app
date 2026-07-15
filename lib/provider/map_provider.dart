16ecd9711d7431c237762ac71ec86889b84c58d3
Daiki Shibata <121758973+d-shibata7069@users.noreply.github.com>
2026-07-15 22:56:09 +0900

    style: satisfy Flutter analyzer import rules

--- lib/provider/map_provider.dart
+++ lib/provider/map_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:instrunment_app/component/flutter_map/packages.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instrunment_app/component/flutter_map/packages.dart';
 
 final markerProvider = StateNotifierProvider<MarkerNotifier, List<Marker>>(
     (ref) => MarkerNotifier());
